70a5659780e3403f08e9361405fb76fe749b8971
Aloïs VINCENT <alois.vincent@ik.me>
2024-03-05 10:24:36 +0100

    Génération des parents + tracé des chemins

--- chemin.py
+++ chemin.py
@@ -4,7 +4,7 @@ class chemin(city):
     def __init__(self, name, cities):
         self.name = name
         self.cities = cities
-        self.score = self.evaluate()
+        self.score = self.evaluate(cities)
 
     #? Distance euclidienne entre deux villes
     def distance(self, city1, city2):
@@ -12,11 +12,11 @@ class chemin(city):
 
 
     #? Attribution du score basé sur la distance, on cherche à maximiser le score
-    def evaluate(self):
+    #TODO : maximiser le score avec une formule 
+    def evaluate(self, cities):
         score = 0
-        for i in range(cities.length()-1):
-            for j in range(start = 1, stop = cities.length()-1):
-                score += self.distance(cities[i], cities[y])
-        score = np.log(score)
+        for i in range(len(cities)-1):
+            for j in range(1, len(cities)-1):
+                score += self.distance(cities[i], cities[j])
         return score
             
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 ### On pourra rentrer le nombre de villes dans l'interface
 ### La grille se calculerait en fonction du nombre de villes
 # Affichage des chemins en temps réél lors du calcul
+# Donner de vrais noms de villes existantes qu'on piocherait dans un tableau par exemple
+
 
 #! Nommage : 
 ## Un individu (parent ou enfant) = un chemin solution, de taille nb_cities
@@ -24,6 +26,9 @@ nb_cities = 30
 
 # Taille de la grille   
 grid_size = 100
+
+#
+nb_parents = 2
 
 # Création de la liste de villes de façon aléatoire
 cities = [city(i, random.randint(0, grid_size), random.randint(0, grid_size))for i in range(nb_cities)]
@@ -43,6 +48,7 @@ xs = [city.x for city in cities]
 ys = [city.y for city in cities]
 
 # Création d'un graphique
+plt.ion
 plt.scatter(xs, ys, c='red', marker='.')
 # Annotations
 for i, name in enumerate(names):
@@ -50,6 +56,48 @@ for i, name in enumerate(names):
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Carte des villes générées aléatoirement')
+#? Garde le graphique ouvert lors de l'exécution du code
+plt.show(block = False)
+
+
+#################################################################################################
+###########################  Génération des parents  ############################################
+#################################################################################################
+
+def generateParents(n, cities):
+    numbers = list(range(nb_cities))
+    tabParent = []
+    for i in range(n):
+        random.shuffle(numbers)
+        listTest = []
+        for j in numbers : 
+            listTest.append(cities[j])
+        tabParent.append(chemin(str(i), listTest))
+    return tabParent
+
+#################################################################################################
+###########################  Affichage des parents  #############################################
+#################################################################################################
+
+#? Test de la génération des parents -> affichage console
+tabParents = generateParents(nb_parents, cities)
+for i in range(len(tabParents)):
+    chemin = tabParents[i].cities
+    print("\nParent " + str(i) + " :" + str(tabParents[i].score))
+    # for j in chemin:
+    #     print(j.toString())
+
+#? Tracer le chemin des parents sur le graphique
+#TODO : transformer en fonction
+# Pour chaque ligne du tableau de parents
+for i in range(len(tabParents)):
+    colors = ['red','blue','green','purple','pink']
+    #On va tracer le chemin
+    chemin = tabParents[i].cities
+    for j in range((nb_cities-1)):
+        plt.plot([chemin[j].x, chemin[j+1].x], [chemin[j].y, chemin[j+1].y], color = colors[i])
+ 
+
+plt.ioff()
 plt.show()
-
-
+plt.pause(0)
