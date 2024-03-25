2e518c2c5adbbe3a04ccf756b22660abb1c95b0a
Aloïs VINCENT <alois.vincent@ik.me>
2024-03-25 21:21:02 +0100

    création d'une fonction generateParents(nb_parents) qui renvoie une liste de nb_parents parents néo-formés

--- V2/main.py
+++ V2/main.py
@@ -3,14 +3,15 @@ from matplotlib import image as mpimpg
 import numpy as np
 import random
 
-###################################
-######## Nouveau départ ###########
-###################################
+################################################
+# Binôme : Aloïs VINCENT et Jean REESE
+# Implémentation d'un algorithme génétique pour résoudre le problème du voyageur de commerces
+################################################
 
 image = mpimpg.imread("V2/map.jpg")
 plt.imshow(image)
 
-#           location   x   y
+#?                      x   y     Localisation
 city_list = np.array([[290,180],  #Paris
                       [390,420],  #Marseille
                       [380,320],  #Lyon
@@ -31,20 +32,23 @@ plt.show()
 #? A partir de la liste de villes, créer 10 chemins "parents"
 #? Ordre aléatoire, mais la première ville doit également être la dernière du chemin
 
-# Initialisation 
-nb_parents = 10
-indexes = list((range(1,nb_cities)))
-populationParent = [None] * (nb_parents)
-orderedList = [[None]] * (nb_cities+1)
+def generateParents(nb_parents) :
+    # Initialisation 
+    indexes = list((range(1,nb_cities)))
+    populationParent = [None] * (nb_parents)
+    orderedList = [[None]] * (nb_cities+1)
 
-for i in range(nb_parents):
-    # On change l'ordre de remplissage
-    random.shuffle(indexes)
+    for i in range(nb_parents):
+        # On change l'ordre de remplissage
+        random.shuffle(indexes)
 
-    # Pour remplir un parent aléatoire
-    for j in range(1,nb_cities) :
-        orderedList[j] = city_list[indexes[j-1]]
-    orderedList[0] = city_list[0]
-    orderedList[-1] = city_list[0]
-    #On ajoute ce parent à la population
-    populationParent[i] = orderedList
+        # Pour remplir un parent aléatoire
+        for j in range(1,nb_cities) :
+            orderedList[j] = city_list[indexes[j-1]]
+        orderedList[0] = city_list[0]
+        orderedList[-1] = city_list[0]
+        # On ajoute ce parent à la population
+        populationParent[i] = orderedList
+
+    # On renvoie un tableau de n parents néo-formés
+    return populationParent
